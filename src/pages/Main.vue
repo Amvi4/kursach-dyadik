739e7b405e5a7c1e1a93f6f69489dbd33f1a024e
Amvi4 <igordyadik@gmail.com>
2025-12-04 17:54:53 +1000

    third commit

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -10,7 +10,7 @@ import { RouterLink } from 'vue-router';
             return {
                 // данные новинок
                 items: [
-                    { title: 'Bianchi AQUILA L DURA ACE DI2 TEAM JUMBO 2021', price: 38484, from: '/src/images/country/italy.png', image: "/src/images/bicycle-bianchi.png", category: 'mountain', stock: 0, },
+                    { title: 'Bianchi AQUILA L DURA ACE DI2 TEAM JUMBO 2021', price: 38484, from: '/assets/images/country/italy.png', image: "/assets/images/bicycle-bianchi.png", category: 'mountain', stock: 0, },
                     { title: 'Trek Verve 2 Disc Lithium Grey HYBD 2022', price: 49300, from:'/src/images/country/usa.png', image: '/src/images/bicycle-trekVerve.png', category: 'mountain', stock: 1,},
                     { title: 'Trek Marlin 7 Matte Nautical Navy Matte Anth ATB 29 2022', price: 15000, from:'/src/images/country/usa.png', image: '/src/images/bicycle-trekMarlin.png', category: 'mountain', stock: 0,},
                 ],
